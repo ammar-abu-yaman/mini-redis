3d48019d7f4c14c93d3c4353069f99290d7685eb
Ammar Abu Yaman <ammar.abu.yaman@gmail.com>
2023-07-24 23:12:31 +0300

    Added the ability to respond to PING commands

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,56 @@
-use std::{net::TcpListener, io::Read};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+    println!("Started server successfully");    
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream ) => {
-                println!("connected");
-                let mut buf = [0u8; 4];
-                stream.read_exact(&mut buf);
-                println!("Recieved {}", String::from_utf8(buf.to_vec()).unwrap());
-                println!("accepted new connection");
+                println!("Established a new Connection");
+                let input = parse_array(&mut stream);
+                if input[0] == "PING" {
+                    write!(stream, "+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn parse_array(stream: &mut TcpStream) -> Vec<String> {
+    let mut result = vec![];
+    let mut buf = [0u8; 256];
+    stream.bytes().next();
+    let array_len: i32 = parse_len(stream.bytes(), &mut buf);
+    for _ in 0..array_len {
+        let string = {
+            stream.bytes().next();
+            let string_len = parse_len(stream.bytes(), &mut buf) as usize;
+            stream.read_exact(&mut buf[..string_len]).unwrap();
+            stream.bytes().next();
+            stream.bytes().next();
+            String::from_utf8(buf[..string_len].to_vec())
+        };
+        result.push(string.unwrap());
+    }
+    result
+}
+
+fn parse_len(mut stream: std::io::Bytes<&mut TcpStream>, buf: &mut [u8; 256]) -> i32 {
+    let mut len = 0usize;
+    while let Some(byte) = stream.next() {
+        let byte = byte.unwrap();
+        if matches!(byte, b'0'..=b'9' | b'-') {
+            buf[len] = byte;
+            len += 1;
+        } else {
+            break;
+        }
+    }
+    stream.next();
+
+    let array_len_str_rep = String::from_utf8(buf[..len].to_vec()).unwrap();
+    array_len_str_rep.parse().unwrap()
 }
